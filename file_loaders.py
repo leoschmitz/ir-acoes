aba85e299d90ef1f516d946b8f1070489d818085
Leonardo Schmitz <laschmitz@gmail.com>
2023-03-10 12:19:46 -0300

    Split loader into module
    Its going to be necessary to load all b3 files also, so its better to
    have a separate file just for file handling

---
+++ file_loaders.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import sys
+import json
+
+logger = logging.getLogger(__name__)
+
+
+def load_input_file(filename):
+    if not os.path.exists(filename):
+        logger.error('Missing %s', filename)
+        sys.exit(1)
+
+    with open(filename) as file:
+        try:
+            stocks = json.loads(file.read())
+        except json.JSONDecodeError as error:
+            logger.info('Invalid json file %s. Fix it and then retry.', filename)
+            sys.exit(1)
+
+    # sanity check
+    for stock, details in stocks.items():
+        logger.info('Checking %s...', stock)
+        assert 'preco-medio' in details
+        assert 'quantidade' in details
+
+    logger.info('...all good!')
+
+    return stocks
+
