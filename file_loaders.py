cd6726dbc81affaa7c3716de6a540eb6ebfb75b4
Leonardo Schmitz <laschmitz@gmail.com>
2023-03-11 16:12:59 -0300

    Parse useful info from b3 file
    get only the columns that will be necessary

--- file_loaders.py
+++ file_loaders.py
@@ -2,7 +2,10 @@ import logging
 import os
 import sys
 import json
+from datetime import datetime
+from enum import IntEnum
 from openpyxl import load_workbook
+
 
 logger = logging.getLogger(__name__)
 
@@ -30,18 +33,54 @@ def load_input_file(filename):
     return stocks
 
 
+class col(IntEnum):
+    date = 0
+    type_ = 1
+    code = 5
+    quantity = 6
+    price = 7
+
+
 def _parse_b3_file(filename):
     logger.info('parsing %s', filename)
     workbook = load_workbook(filename)
+
     worksheet = workbook['Negociação']
     rows = iter(worksheet.rows)
-    logger.info('Loading -> %s', ', '.join([title.value for title in next(rows)]))
+    titles = [title.value for title in next(rows)]
+    logger.info('Loading -> %s', ', '.join(titles))
+
+    assert 'Data do Negócio' == titles[col.date]
+    assert 'Tipo de Movimentação' == titles[col.type_]
+    assert 'Código de Negociação' == titles[col.code]
+    assert 'Quantidade' == titles[col.quantity]
+    assert 'Preço' == titles[col.price]
+
+    stocks = {}
     for row in rows:
-        for cell in row:
-            print(cell.value)
-        break
+        operation = [cell.value for cell in row]
+
+        op_type = operation[col.type_].upper()
+        assert op_type in ('VENDA', 'COMPRA')
+
+        date = operation[col.date]
+        date = datetime.strptime(operation[col.date], '%d/%m/%Y').date()
+
+        quantity = operation[col.quantity]
+        assert isinstance(quantity, int)
+
+        price = float(operation[col.price])
+
+        values = [op_type, date, quantity, price]
+
+        code = operation[col.code]
+        if code not in stocks:
+            stocks[code] = [values]
+        else:
+            stocks[code].append(values)
+
     workbook.close()
-    return None
+    return stocks
 
 
 def load_b3_file():
