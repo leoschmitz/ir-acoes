7d71a251e6fa982bfa1ba05714e6de8acee211c6
Leonardo Schmitz <laschmitz@gmail.com>
2023-03-12 18:31:46 -0300

    Improve data structures
    Year class has methods to get monthly or yearly averages

--- check.py
+++ check.py
@@ -1,35 +1,13 @@
 import logging
 from data import Buy
+from data import Year
 
 logger = logging.getLogger(__name__)
 
 
-def _average(operations):
-    buy_sum = 0.0
-    sell_sum = 0.0
-    buy_qtty = 0
-    sell_qtty = 0
-    total_ops = 0
-    for operation in operations:
-        total_ops += 1
-        if isinstance(operation, Buy):
-            buy_sum += operation.total
-            buy_qtty += operation.quantity
-            continue
-
-        sell_sum += operation.total
-        sell_qtty += operation.quantity
-
-    logger.info('Total operations %s', total_ops)
-
-    buy_qtty = 1 if not buy_qtty else buy_qtty
-    sell_qtty = 1 if not sell_qtty else sell_qtty
-
-    return {'buy': buy_sum / buy_qtty, 'sell': sell_sum / sell_qtty}
-
-
 def averages(stocks):
     for stock, operations in stocks.items():
+        logger.info('--------------')
         logger.info('Averaging %s', stock)
-        av = _average(operations)
-        logger.info('buy %s sell %s', av['buy'], av['sell'])
+        year = Year.from_operations(operations)
+        logger.info('buy %s sell %s', year.average('BUY'), year.average('SELL'))
--- data.py
+++ data.py
@@ -1,5 +1,12 @@
+import logging
 from dataclasses import dataclass
+from dataclasses import field
 from datetime import datetime
+from typing import ClassVar
+from typing import List
+
+logger = logging.getLogger(__name__)
+
 
 @dataclass
 class Operation:
@@ -19,3 +26,82 @@ class Buy(Operation):
 
 class Sell(Operation):
     """Buy op"""
+
+
+@dataclass
+class MonthlyBucket:
+    ops: List[Operation] = field(default_factory=list)
+
+    @property
+    def total(self):
+        sum_ = 0.0
+        for op in self.ops:
+            sum_ += op.total
+        return sum_
+
+    @property
+    def quantity(self):
+        sum_ = 0
+        for op in self.ops:
+            sum_ += op.quantity
+        return sum_
+
+    @property
+    def average(self):
+        quantity = self.quantity
+        if not quantity:
+            return 0.0
+
+        return self.total / quantity
+
+
+@dataclass
+class Monthly:
+    month: int
+    buy: MonthlyBucket = field(default_factory=MonthlyBucket)
+    sell: MonthlyBucket = field(default_factory=MonthlyBucket)
+
+
+def build_year():
+    return [Monthly(i) for i in range(1, 13)]
+
+
+@dataclass
+class Year:
+    stock: str
+    months: List[Monthly] = field(default_factory=build_year)
+
+    @classmethod
+    def from_operations(cls, operations):
+        logger.info('Total operations %s', len(operations))
+
+        year_ops = cls(operations[0].stock)
+
+        for operation in operations:
+            index_month = operation.date.month - 1
+            if isinstance(operation, Buy):
+                year_ops.months[index_month].buy.ops.append(operation)
+                continue
+            year_ops.months[index_month].sell.ops.append(operation)
+
+        return year_ops
+
+
+    def total(self, operation_type):
+        sum_ = 0.0
+        for month in self.months:
+            sum_ += month.buy.total if operation_type == 'BUY' else month.sell.total
+        return sum_
+
+    def quantity(self, operation_type):
+        sum_ = 0
+        for month in self.months:
+            sum_ += month.buy.quantity if operation_type == 'BUY' else month.sell.quantity
+        return sum_
+
+    def average(self, operation_type):
+        quantity = self.quantity(operation_type)
+        if not quantity:
+            return 0.0
+
+        return self.total(operation_type) / quantity
