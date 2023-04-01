c1e672a23740036224ce223db0dd944e0b123adf
Leonardo Schmitz <laschmitz@gmail.com>
2023-04-01 12:30:15 -0300

    Add year op tests
    adding tests related to the year class

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,20 @@
+from calendar import isleap
+from calendar import mdays
 from datetime import datetime
+from datetime import timedelta
 from unittest import TestCase
 
 from taxes import data
 
 
-def _now():
-    return datetime.now().date()
+def _now(delta_month=0):
+    now_ = datetime.now().date()
+    if not delta_month:
+        return now_
+
+    month = (now_.month + delta_month) % 12
+    days = mdays[month] + (month == 2 and isleap(now_.year))
+    return now_ + timedelta(days=days)
 
 
 class TestOperation(TestCase):
@@ -67,3 +76,59 @@ class TestMonthOperations(TestCase):
         self.month.add(data.Sell(stock='STOC4', quantity=100, price=1.0, date=_now()))
         self.assertEqual(len(self.month.buy), 2)
         self.assertEqual(len(self.month.sell), 1)
+
+    def test_adding_invalid_operation(self):
+        with self.assertRaises(TypeError):
+            self.month.add(
+                data.Operation(stock='STOC4', quantity=100, price=1.0, date=_now())
+            )
+
+
+class TestYearOpsSingleStock(TestCase):
+    # issue #1 will fix this
+    def test_same_month_buy_sell(self):
+        with self.assertRaises(AssertionError):
+            data.YearOperations('STOC4', 2023, {}, [
+                data.Buy(stock='STOC4', quantity=100, price=1.0, date=_now()),
+                data.Sell(stock='STOC4', quantity=100, price=1.0, date=_now())
+            ])
+
+    def test_calculate_loss_only_buy_ops(self):
+        year = data.YearOperations('STOC4', 2023, {}, [
+            data.Buy(stock='STOC4', quantity=100, price=1.0, date=_now()),
+            data.Buy(stock='STOC4', quantity=100, price=1.0, date=_now(delta_month=1))
+        ])
+        year.calculate_loss_or_profit()
+        self.assertEqual(year.accum_loss, 0.0)
+
+    def test_calculate_loss_with_profit(self):
+        year = data.YearOperations('STOC4', 2023, {}, [
+            data.Buy(stock='STOC4', quantity=100, price=1.0, date=_now()),
+            data.Sell(stock='STOC4', quantity=100, price=2.0, date=_now(delta_month=1))
+        ])
+        year.calculate_loss_or_profit()
+        self.assertEqual(year.accum_loss, 0.0)
+
+    def test_calculate_loss_successfully(self):
+        year = data.YearOperations('STOC4', 2023, {}, [
+            data.Buy(stock='STOC4', quantity=100, price=2.0, date=_now()),
+            data.Sell(stock='STOC4', quantity=100, price=1.0, date=_now(delta_month=1))
+        ])
+        year.calculate_loss_or_profit()
+        self.assertEqual(year.accum_loss, -100.0)
+
+    def test_calculate_profit_only_buy_ops(self):
+        year = data.YearOperations('STOC4', 2023, {}, [
+            data.Buy(stock='STOC4', quantity=100, price=1.0, date=_now()),
+            data.Buy(stock='STOC4', quantity=100, price=1.0, date=_now(delta_month=1))
+        ])
+        year.calculate_loss_or_profit()
+        self.assertEqual(year.tax_free_profit, 0.0)
+
+    def test_calculate_profit_successfully(self):
+        year = data.YearOperations('STOC4', 2023, {}, [
+            data.Buy(stock='STOC4', quantity=100, price=1.0, date=_now()),
+            data.Sell(stock='STOC4', quantity=100, price=2.0, date=_now(delta_month=1))
+        ])
+        year.calculate_loss_or_profit()
+        self.assertEqual(year.tax_free_profit, 100.0)
